45c43d36bbf75203d9e6aabdebc6ad569d3c66fe
Seungkyun <seungkyun.h@gmail.com>
2021-09-27 15:26:24 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/seungkyun/Login/Login.js
+++ src/pages/seungkyun/Login/Login.js
@@ -4,6 +4,23 @@ import '../../../styles/reset.scss';
 import './Login.scss';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      id: '',
+      pw: '',
+    };
+  }
+  handleIdInput = e => {
+    this.setState({
+      id: e.target.value,
+    });
+  };
+  handlePwInput = e => {
+    this.setState({
+      pw: e.target.value,
+    });
+  };
   goToMain = () => {
     this.props.history.push('../main/main');
   };
@@ -19,12 +36,14 @@ class Login extends React.Component {
               placeholder="전화번호, 사용자 이름 또는 이메일"
               id="idfield"
               className="textfield"
+              onChange={this.handleIdInput}
             />
             <input
               type="password"
               placeholder="비밀번호"
               id="pwfield"
               className="textfield"
+              onChange={this.handlePwInput}
             />
             <button
               className="submitButton"
