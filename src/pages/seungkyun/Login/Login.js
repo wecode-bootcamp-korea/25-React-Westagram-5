935911112c540de268652523804cabefb0b61022
Seungkyun <seungkyun.h@gmail.com>
2021-10-01 10:55:46 +0900

    Login Page 수정

--- src/pages/seungkyun/Login/Login.js
+++ src/pages/seungkyun/Login/Login.js
@@ -8,34 +8,30 @@ class Login extends React.Component {
     this.state = {
       id: '',
       pw: '',
-      isBtnActive: false,
     };
   }
 
-  handleIdInput = e => {
+  handleInput = e => {
+    const { name, value } = e.target;
     this.setState({
-      id: e.target.value,
+      [name]: value,
     });
   };
 
-  handlePwInput = e => {
-    this.setState({
-      pw: e.target.value,
-    });
-  };
-
-  handleBtn = () => {
-    const { id, pw } = this.state;
-    id.includes('@') && pw.length > 5
-      ? this.setState({ isBtnActive: true })
-      : this.setState({ isBtnActive: false });
+  pressEnter = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      this.goToMain();
+    }
   };
 
   goToMain = () => {
-    this.props.history.push('/main-seungkyun');
+    const isBtnValid = this.state.id.includes('@') && this.state.pw.length > 5;
+    isBtnValid && this.props.history.push('/main-seungkyun');
   };
 
   render() {
+    const isBtnValid = this.state.id.includes('@') && this.state.pw.length > 5;
     return (
       <main className="login-seungkyun">
         <div className="container">
@@ -44,26 +40,23 @@ class Login extends React.Component {
             <input
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
-              id="idfield"
+              name="id"
               className="textfield"
-              onKeyUp={this.handleBtn}
-              onChange={this.handleIdInput}
+              onKeyUp={this.pressEnter}
+              onChange={this.handleInput}
             />
             <input
               type="password"
               placeholder="비밀번호"
-              id="pwfield"
+              name="pw"
               className="textfield"
-              onKeyUp={this.handleBtn}
-              onChange={this.handlePwInput}
+              onKeyUp={this.pressEnter}
+              onChange={this.handleInput}
             />
             <button
-              className={
-                this.state.isBtnActive ? 'submitButton btnOn' : 'submitButton'
-              }
-              disabled={!this.state.isBtnActive}
+              className={`submitButton ${isBtnValid ? 'btnOn' : ''}`}
+              disabled={!isBtnValid}
               onClick={this.goToMain}
-              id="submitButton"
             >
               로그인
             </button>
